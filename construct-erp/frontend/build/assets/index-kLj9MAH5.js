bbf69a305060f2d9a61886208f430b3f95517cb7
BCIM IT <admin@bcimengineering.onmicrosoft.com>
2026-07-21 11:55:05 +0530

    chore(build): rebuild frontend/build after merge (clean up rename/rename cruft, ship stage-label fix)

--- construct-erp/frontend/build/assets/index-kLj9MAH5.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:construct-erp/frontend/build/assets/index-kLj9MAH5.js
-import{b8 as i}from"./index-CcCHka0_.js";import"./vendor-react-C3P8e1DV.js";import"./vendor-data-693MZDSQ.js";import"./vendor-ui-BL8pBBi4.js";import"./vendor-icons-Bb2wc4Hz.js";import"./vendor-forms-gVMGBE4b.js";const n=i("PushNotifications",{});export{n as PushNotifications};
-========
-import{b8 as i}from"./index-DxxJhgV5.js";import"./vendor-react-C3P8e1DV.js";import"./vendor-data-693MZDSQ.js";import"./vendor-ui-BL8pBBi4.js";import"./vendor-icons-Bb2wc4Hz.js";import"./vendor-forms-gVMGBE4b.js";const n=i("PushNotifications",{});export{n as PushNotifications};
->>>>>>>> origin/main:construct-erp/frontend/build/assets/index-BSESq2H1.js
